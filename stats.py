fcf061c193215fd038d72a2714f9191eb9d85e6c
KeshtheGoblin <sstout12@wgu.edu>
2026-04-08 06:08:59 -0500

    Added individual character counter

--- stats.py
+++ stats.py
@@ -6,8 +6,14 @@ def get_book_text(path_to_file):
 def get_num_words(path_to_file):
     book_text = get_book_text(path_to_file)
     return len(book_text.split())
-
+  
 def get_char_count(path_to_file):
     book_text = get_book_text(path_to_file)
-    char_count = len([c for c in book_text if c != " " and c != "\n"])
-    return char_count
+    chars = [c.lower() for c in book_text if c != " " and c != "\n"]
+    duplicates = {}
+    for item in chars:
+        if item in duplicates:
+            duplicates[item] += 1
+        else:
+            duplicates[item] = 1
+    return sorted({key: value for key, value in duplicates.items() if value > 1}.items())
