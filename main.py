e875c358612b847778d3ec935e56e446874e22a3
KeshtheGoblin <sstout12@wgu.edu>
2026-04-08 06:14:21 -0500

    Updated text in main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     num_words = get_num_words("books/frankenstein.txt")
     print(f"Found {num_words} total words.")
     char_count = get_char_count("books/frankenstein.txt")
-    print(f"Found {char_count} total characters.")
+    print(f"{char_count} individual characters (not counting spaces or newlines).")
